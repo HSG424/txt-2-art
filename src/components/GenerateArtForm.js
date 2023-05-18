8d008e42f454f7f63ad09bc83827fca293b8ac60
HSG424 <mrhan998@gmail.com>
2023-05-18 20:50:03 +0700

    logo and tailwind styling

--- src/components/GenerateArtForm.js
+++ src/components/GenerateArtForm.js
@@ -25,19 +25,55 @@ function GenerateArtForm(props) {
     console.log("data@: ", data);
   }
 
-  return (
-    <form onSubmit={submitHandler}>
-      <input
-        type="text"
-        id="text"
-        value={text}
-        placeholder="Please keep your searches clean"
-        onChange={textChangeHandler}
-        maxLength={50}
-        disabled={false}
-      />
+  // common tailwind styling
+  const textColor = "text-gray-100";
+  const flexCenter = "flex justify-center";
+  const inputStyle =
+    "text-[15px] text-center w-60 bg-gray-200 text-gray-900 rounded-md pt-[8px] pb-[9px] px-[7px] focus:ring-4 focus:ring-blue-300";
 
-      <button disabled={!text.length > 0}>Generate!</button>
+  return (
+    <form onSubmit={submitHandler} className="flex flex-col mt-[134px]">
+      <div className={`${flexCenter} mb-1`}>
+        <label className={textColor} for="mode">
+          Choose mode:
+        </label>
+      </div>
+
+      <div className={`${flexCenter} mb-[18px]`}>
+        <select name="mode" id="mode" className={inputStyle}>
+          <option value="default">Text To Image (default)</option>
+          <option value="impressionism">Impressionism</option>
+          <option value="surreal">Surreal Graphics</option>
+        </select>
+      </div>
+
+      <div className={`${flexCenter} mb-1`}>
+        <label className={textColor} for="text">
+          Creation text:
+        </label>
+      </div>
+
+      <div className={`${flexCenter} mb-[23px]`}>
+        <input
+          className={inputStyle}
+          type="text"
+          id="text"
+          value={text}
+          placeholder="Enter something clever..."
+          onChange={textChangeHandler}
+          maxLength={50}
+          disabled={false}
+        />
+      </div>
+
+      <div className={flexCenter}>
+        <button
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 rounded-lg px-[21px] pt-[12px] pb-[11px]"
+          disabled={!text.length > 0}
+        >
+          Generate
+        </button>
+      </div>
     </form>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,28 @@
+import { Fragment } from "react";
+import Image from "next/image";
 import GenerateArtForm from "../components/GenerateArtForm";
+import { Montserrat } from "next/font/google";
+
+const mont = Montserrat({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main>
-      <GenerateArtForm />
-    </main>
+    <Fragment>
+      <header
+        className={`flex justify-center gap-x-[73px] ${mont.className} text-base`}
+      >
+        <Image
+          src="/logo.png"
+          alt="Logo"
+          className="mt-[69px] mb-16"
+          width={270}
+          height={1}
+          priority
+        />
+        <GenerateArtForm />
+      </header>
+      <main></main>
+      <footer></footer>
+    </Fragment>
   );
 }
