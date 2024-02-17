0468dc8fd3f6c6e30cbc956da85addbfdb2f1baa
HSG424 <mrhan998@gmail.com>
2024-02-17 03:00:17 -0600

    styling

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,7 +6,7 @@ export function Main() {
   const { mode, generatedImage, isLoading, requestError } = useContext(Context);
 
   const textStyle =
-    "w-[33.4375rem] text-center text-base leading-[1.875rem] border-b border-gray-700 pb-[1.6875rem] mb-[0.1875rem] px-[2.0625rem] xs:px-[1rem]";
+    "w-[33.4375rem] text-center text-base leading-[1.875rem] border-b border-gray-700 pb-[2.8125rem] mb-[0.1875rem] px-[2.0625rem] xs:px-[1rem]";
 
   return requestError ? (
     <Error
